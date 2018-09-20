a39aef3fa1336073411bbb051cd5a328a1b413c3
Rakesh <rakesh22717@gmail.com>
2018-09-21 01:47:15 +0530

    Initial commit

---
+++ app/src/main/java/com/carbyke/carbyke/Home.java
@@ -0,0 +1,162 @@
+package com.carbyke.carbyke;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.squareup.picasso.Picasso;
+
+public class Home extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
+
+    private ImageView background_iv;
+    private ImageButton car_ib, bike_ib;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+        background_iv = findViewById(R.id.ch_background_iv);
+        car_ib = findViewById(R.id.ch_car_ib);
+        bike_ib = findViewById(R.id.ch_bike_ib);
+
+       // setBackGroundImage(); //setting background image
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(""); // setting title to null
+        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        car_ib.setOnClickListener(this);
+        bike_ib.setOnClickListener(this);
+    }
+
+//    on start
+    public void onStart(){
+        super.onStart();
+
+    }
+//    on Start
+
+
+
+
+//    set background image
+    private void setBackGroundImage() {
+        Picasso.with(Home.this)
+                .load(R.drawable.home_fade_background)
+                .fit()
+                .into(background_iv);
+    }
+//    set background image
+
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+//    onclick
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id){
+            case R.id.ch_car_ib:
+                startActivity(new Intent(Home.this, SearchCar.class));
+                break;
+            case R.id.ch_bike_ib:
+                break;
+        }
+
+    }
+//    onclick
+
+
+    //end
+}
---
+++ app/src/main/java/com/carbyke/carbyke/SearchCar.java
@@ -0,0 +1,261 @@
+package com.carbyke.carbyke;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.kunzisoft.switchdatetime.SwitchDateTimeDialogFragment;
+import com.squareup.picasso.Picasso;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class SearchCar extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView background_iv;
+    private ImageButton back_btn;
+    private TextView pick_up_time_tv, drop_off_time_tv, calculated_days_or_hours;
+    Date date_to_be_set_for_drop_off, date_to_be_set_for_pick_up;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_car);
+
+        background_iv = findViewById(R.id.sc_background_iv);
+        back_btn = findViewById(R.id.sc_back_b);
+        pick_up_time_tv = findViewById(R.id.sc_pick_up_time_tv);
+        drop_off_time_tv = findViewById(R.id.sc_drop_off_time_tv);
+        calculated_days_or_hours = findViewById(R.id.sc_calculated_days_tv);
+
+        date_to_be_set_for_pick_up = new Date();
+
+        //setImages();
+
+        back_btn.setOnClickListener(this);
+        pick_up_time_tv.setOnClickListener(this);
+        drop_off_time_tv.setOnClickListener(this);
+
+    }
+
+//    setting images
+    private void setImages() {
+        Picasso.with(SearchCar.this)
+                .load(R.drawable.search_car_background_fade)
+                .fit()
+                .centerCrop()
+                .into(background_iv);
+    }
+    //    setting images
+
+
+//    onclick
+    @Override
+    public void onClick(View view) {
+
+        int id = view.getId();
+        switch (id){
+//            back button
+            case R.id.sc_back_b:
+                super.onBackPressed();
+                break;
+
+//                pick up date and time
+            case R.id.sc_pick_up_time_tv:
+                final SwitchDateTimeDialogFragment dateTimeDialogFragment = new SwitchDateTimeDialogFragment();
+                dateTimeDialogFragment.setHighlightAMPMSelection(true);
+                dateTimeDialogFragment.startAtCalendarView();
+                dateTimeDialogFragment.setCancelable(false);
+                dateTimeDialogFragment.setMinimumDateTime(new Date());
+
+                //adding one more day
+                final Calendar calendar = Calendar.getInstance();
+                if (TextUtils.isEmpty(pick_up_time_tv.getText().toString())){ // if date not set then pick next date by default otherwise choose date chosen previously
+                    calendar.setTime(date_to_be_set_for_pick_up);
+                    calendar.add(Calendar.DATE, 1);
+                    date_to_be_set_for_pick_up = calendar.getTime();
+                    dateTimeDialogFragment.setDefaultDateTime(date_to_be_set_for_pick_up);
+                    dateTimeDialogFragment.setDefaultHourOfDay(12);
+                    dateTimeDialogFragment.setDefaultMinute(0);
+                }
+                else {
+                    dateTimeDialogFragment.setDefaultDateTime(date_to_be_set_for_pick_up);
+                }
+                handlePickUpTime(dateTimeDialogFragment);
+                break;
+                //pick up date time
+
+            //                drop off date and time
+            case R.id.sc_drop_off_time_tv:
+                if (TextUtils.isEmpty(pick_up_time_tv.getText().toString())){
+                    Toast.makeText(this, "Please Select Pick up time first", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    handleDropOffTime();
+                }
+                break;
+                //drop off date time
+        }
+        //switch
+    }
+    //   onclick
+
+
+//    pick up time
+    private void handlePickUpTime(final SwitchDateTimeDialogFragment dateTimeDialogFragment) {
+        dateTimeDialogFragment.setOnButtonClickListener(new SwitchDateTimeDialogFragment.OnButtonClickListener() {
+            @SuppressLint({"SetTextI18n", "SimpleDateFormat"})
+            @Override
+            public void onPositiveButtonClick(Date date) {
+
+                if (checkIfPickTimeIsNotLessThanCurrentTime(date)){
+                    pick_up_time_tv.callOnClick();
+                    Toast.makeText(SearchCar.this, "Pick time can not be earlier than current time...", Toast.LENGTH_LONG).show();
+                    //if (TextUtils.isEmpty(pick_up_time_tv.getText().toString())) date_to_be_set_for_pick_up = new Date();
+                    return;
+                }
+
+                if (!TextUtils.isEmpty(drop_off_time_tv.getText().toString())){ // if pick time is greater than drop off time then change drop off time accordingly
+                    if (checkIfPickTimeIsNotGreaterThanDropOffTime(date)){
+                        Calendar calendar = Calendar.getInstance();
+                        calendar.setTime(date);
+                        calendar.add(Calendar.DATE, 1);
+                        date_to_be_set_for_drop_off = calendar.getTime();
+
+                        SimpleDateFormat day, dateMonthYear, time;
+                        day = new SimpleDateFormat("EEEE");
+                        dateMonthYear = new SimpleDateFormat("MMM dd yyyy");
+                        time = new SimpleDateFormat("hh:mm aaa");
+                        drop_off_time_tv.setBackgroundResource(R.drawable.corner_rectangle_rent_home);
+                        drop_off_time_tv.setText(day.format(date_to_be_set_for_drop_off)+"\n"+dateMonthYear.format(date_to_be_set_for_drop_off)+"\n"+time.format(date_to_be_set_for_drop_off));
+
+                        Toast.makeText(SearchCar.this, "drop off time updated.", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+
+                SimpleDateFormat day, dateMonthYear, time;
+                day = new SimpleDateFormat("EEEE");
+                dateMonthYear = new SimpleDateFormat("MMM dd yyyy");
+                time = new SimpleDateFormat("hh:mm aaa");
+                pick_up_time_tv.setBackgroundResource(R.drawable.corner_rectangle_rent_home);
+                pick_up_time_tv.setText(day.format(date)+"\n"+dateMonthYear.format(date)+"\n"+time.format(date));
+                date_to_be_set_for_pick_up = date;
+
+
+                //handling drop off time
+                drop_off_time_tv.setCompoundDrawablesWithIntrinsicBounds(getResources().getDrawable(R.drawable.ic_time_end),null,null,null);
+                drop_off_time_tv.setHintTextColor(getResources().getColor(R.color.black90));
+                drop_off_time_tv.setEnabled(true);
+                if (TextUtils.isEmpty(drop_off_time_tv.getText().toString())) {
+                    date_to_be_set_for_drop_off = date;
+                }
+
+                if (!TextUtils.isEmpty(pick_up_time_tv.getText().toString()) && !TextUtils.isEmpty(drop_off_time_tv.getText().toString())){
+                    SetCalculatedDaysOrHours();
+                }
+            }
+
+            private boolean checkIfPickTimeIsNotLessThanCurrentTime(Date date) {
+                return date.compareTo(new Date()) < 0;
+            }
+
+            private boolean checkIfPickTimeIsNotGreaterThanDropOffTime(Date date) {
+                return date.compareTo(date_to_be_set_for_drop_off) > 0;
+            }
+
+            @Override
+            public void onNegativeButtonClick(Date date) {
+                date_to_be_set_for_pick_up = new Date();
+                // Date is get on negative button click
+            }
+
+        });
+        dateTimeDialogFragment.show(getSupportFragmentManager(), "dialog_time");
+    }
+//    pick up time
+
+
+//    drop off time
+    private void handleDropOffTime() {
+        final SwitchDateTimeDialogFragment dateTimeDialogFragment = new SwitchDateTimeDialogFragment();
+        dateTimeDialogFragment.setHighlightAMPMSelection(true);
+        dateTimeDialogFragment.startAtCalendarView();
+        dateTimeDialogFragment.setMinimumDateTime(date_to_be_set_for_pick_up);
+
+        final Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date_to_be_set_for_drop_off);
+        if (TextUtils.isEmpty(drop_off_time_tv.getText().toString())) calendar.add(Calendar.DATE, 1);
+
+        Date d = calendar.getTime();
+
+        dateTimeDialogFragment.setDefaultDateTime(d);
+        if (TextUtils.isEmpty(drop_off_time_tv.getText().toString())){
+            dateTimeDialogFragment.setDefaultHourOfDay(12);
+            dateTimeDialogFragment.setDefaultMinute(0);
+        }
+
+
+        dateTimeDialogFragment.setOnButtonClickListener(new SwitchDateTimeDialogFragment.OnButtonClickListener() {
+            @SuppressLint({"SetTextI18n", "SimpleDateFormat"})
+            @Override
+            public void onPositiveButtonClick(Date date) {
+
+                if (checkIfDropOffTimeIsNotLessThanPickUpTime(date)){
+                    drop_off_time_tv.callOnClick();
+                    Toast.makeText(SearchCar.this, "Drop off time can not be earlier than Pick up Time...", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                SimpleDateFormat day, dateMonthYear, time;
+                day = new SimpleDateFormat("EEEE");
+                dateMonthYear = new SimpleDateFormat("MMM dd yyyy");
+                time = new SimpleDateFormat("hh:mm aaa");
+                drop_off_time_tv.setBackgroundResource(R.drawable.corner_rectangle_rent_home);
+                drop_off_time_tv.setText(day.format(date)+"\n"+dateMonthYear.format(date)+"\n"+time.format(date));
+
+                date_to_be_set_for_drop_off = date; // saving previous selected date
+                if (!TextUtils.isEmpty(pick_up_time_tv.getText().toString()) && !TextUtils.isEmpty(drop_off_time_tv.getText().toString())){
+                    SetCalculatedDaysOrHours();
+                }
+
+            }
+
+            private boolean checkIfDropOffTimeIsNotLessThanPickUpTime(Date date) {
+                return date.compareTo(date_to_be_set_for_pick_up) < 0;
+            }
+
+
+            @Override
+            public void onNegativeButtonClick(Date date) {
+                // Date is get on negative button click
+            }
+        });
+        dateTimeDialogFragment.show(getSupportFragmentManager(), "dialog_time");
+    }
+//    drop off time
+
+    private void SetCalculatedDaysOrHours(){
+        try {
+             int time;
+             String days, hours, minutes;
+             time = (int) TimeUnit.MINUTES.convert(date_to_be_set_for_drop_off.getTime() - date_to_be_set_for_pick_up.getTime(), TimeUnit.MILLISECONDS);
+             days = String.valueOf(time/(24*60));
+             hours = String.valueOf((time%(24*60)) / 60);
+             minutes = String.valueOf((time%(24*60)) % 60);
+
+             calculated_days_or_hours.setVisibility(View.VISIBLE);
+             calculated_days_or_hours.setText("Note: Car will be rented for "+days+" days, "+ hours+" hours and "+minutes+" minutes.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+//    end
+}
